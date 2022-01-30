6fdf7c1897c41fdcfa287062164a5a5dedafa7ff
erkutr <erkuturkyilmaz@gmail.com>
2022-01-30 18:44:59 +0300

    Added photo number filter

--- App.js
+++ App.js
@@ -6,24 +6,25 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
   TouchableOpacity,
   FlatList,
-  StatusBar,
-  Alert
+  Modal
 } from "react-native";
 import NavigationBar from 'react-native-navbar';
+import { Picker } from "@react-native-picker/picker";
 
 
 function App() {
   const [data, setData] = React.useState([]);
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState("10");
 
   const getImage = async () => {
     var page = 0;
     page = Math.floor(Math.random() * 35);
     try {
       const response = await fetch(
-        "https://picsum.photos/v2/list?page=" + page.toString() + "&limit=10"
+        "https://picsum.photos/v2/list?page=" + page.toString() + "&limit=" + selectedValue + ""
       );
       const json = await response.json();
       setData(json);
@@ -56,9 +57,45 @@ function App() {
     handler: () => getImage(),
   };
 
+  const leftButtonConfig = {
+    title: "Filter",
+    handler: () => setModalVisible(true),
+  }
+
   return (
     <View style={styles.app}>
-      <NavigationBar title={titleConfig} style={styles.navbar} rightButton={rightButtonConfig} >
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.modalView}>
+              <Text style={{position: 'relative', paddingTop: 2, fontWeight: 'bold'}}>Select photo number:</Text>
+                <Picker
+                    selectedValue={selectedValue}
+                    style={{ height: 50, width: 150 }}
+                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                >
+                    <Picker.Item label="1" value="1" />
+                    <Picker.Item label="2" value="2" />
+                    <Picker.Item label="3" value="3" />
+                    <Picker.Item label="4" value="4" />
+                    <Picker.Item label="5" value="5" />
+                    <Picker.Item label="6" value="6" />
+                    <Picker.Item label="7" value="7" />
+                    <Picker.Item label="8" value="8" />
+                    <Picker.Item label="9" value="9" />
+                    <Picker.Item label="10" value="10" />
+                </Picker>
+                <Button color={'orange'} onPress={() => setModalVisible(false)} title="OK"></Button>
+            </View>
+          </View>
+        </Modal>
+      <NavigationBar title={titleConfig} style={styles.navbar} rightButton={rightButtonConfig} leftButton={leftButtonConfig} >
       </NavigationBar>
       <View style={styles.imageContainer}>
         <FlatList
@@ -80,7 +117,8 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: '#F5F5DC'
   },
   image: {
     width: 300,
@@ -88,16 +126,36 @@ const styles = StyleSheet.create({
     alignSelf: "center"
   },
   btn: {
-    height: 100,
-    width: 100,
-    alignSelf: "auto"
+    paddingBottom: 2,
   },
   navbar: {
-    backgroundColor: "#fff",
+    backgroundColor: "#F5F5DC",
   },
   itemText: {
     fontWeight: "bold"
-  }
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#F5F5DC",
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    alignSelf: 'center',
+    height: 200,
+    width: 350
+  },
+  centeredView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
 });
 
 export default App;
